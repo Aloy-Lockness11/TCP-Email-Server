ebab624113d0d77be26a4ca9384b9b3eb25a52c1
aloysius <aloysiuspacheco2003@gmail.com>
2025-05-03 14:10:06 +0100

    feat: implement email sending and retrieval commands; add handleSendEmail and handleGetEmails methods in ClientHandler

--- src/main/java/utils/Protocols/UserProtocol.java
+++ src/main/java/utils/Protocols/UserProtocol.java
@@ -8,9 +8,11 @@ public class UserProtocol {
 
     // RESPONSE CODES
     public static final String SUCCESS = "SUCCESS";
-    public static final String INVALID_FORMAT = "INVALID_FORMAT";
     public static final String INVALID_DETAILS = "INVALID_DETAILS";
     public static final String USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS";
     public static final String NO_USER = "NO_USER";
     public static final String INVALID_CREDENTIALS = "INVALID_CREDENTIALS";
+
+    // WRONG REQUEST FORMAT CODES
+    public static final String INVALID_FORMAT = "INVALID_FORMAT";
 }
